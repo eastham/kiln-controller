396a9b4e59817f15158d25dc947505f37babca8f
Paul Eastham <eastham@gmail.com>
2025-09-23 07:12:41 -0700

    more baud

--- lib/tft_display.py
+++ lib/tft_display.py
@@ -69,7 +69,7 @@ class TFTDisplay(threading.Thread):
                 cs=cs_pin,
                 dc=dc_pin,
                 rst=reset_pin,
-                baudrate=16000000   # Faster speed for better responsiveness
+                baudrate=60000000
             )
 
             # Add initialization delay and explicit reset
